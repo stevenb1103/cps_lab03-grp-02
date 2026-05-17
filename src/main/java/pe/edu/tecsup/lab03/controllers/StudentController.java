217c89f3d625fc4accd8fb5c224137c76cd202cf
Responsable <steven.bernaola@tecsup.edu.pe>
2026-05-17 12:22:42 -0500

    Responsable merge StudentController

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,12 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+    // Punto de entrada de la petición de estudiantes
+    public void getRequest() {
+        System.out.println("Receiving API request for students...");
+    }
+    // Eliminar la petición de un estudiante por su ID
+    public void deleteRequest() {
+        System.out.println("Receiving delete API request...");
+    }
 }
